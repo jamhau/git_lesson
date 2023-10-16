73200a7d9dac35a2fbce4d1092ab0bf4a13127c6
James <jamhau200@gmail.com>
2023-10-16 18:18:45 +0200

    Normal dev

--- demo_scprit.R
+++ demo_scprit.R
@@ -1,10 +1,10 @@
-setwd("/Users/jameshaugen/Documents/git_lesson")
-
+setwd("/Users/jameshaugen/Documents/git_lesson")2
 x <- rnorm(1000)
-y <- rnorm(1000)
+y <- rnorm(200)
 
 
 # Important feauture  323
 
+# Devemelopent
 
 # we add a new featurtere dasdsad dw
